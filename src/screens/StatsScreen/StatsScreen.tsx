3c53c2d70c397cd266281066f4d8f024141230bc
dejv0ld <david_johnson_1992@hotmail.com>
2024-05-06 21:42:30 +0200

    added ract native chart kit

--- src/screens/StatsScreen/StatsScreen.tsx
+++ src/screens/StatsScreen/StatsScreen.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   View,
   Text,
   StyleSheet,
-  TextInput,
-  Button,
   Dimensions,
   ActivityIndicator
 } from 'react-native';
-import { LineChart } from 'react-native-gifted-charts';
+import { LineChart } from 'react-native-chart-kit';
 import { useGetAllExercisesQuery } from '../../store/api/sessionsApi';
 import { StatsStackParamList } from '../../types/navigationType';
 import { RouteProp } from '@react-navigation/native';
@@ -34,7 +32,30 @@ export const StatsScreen: React.FC<Props> = ({ route }) => {
         .filter((ex) => ex.name.toLowerCase() === exercise.toLowerCase())
         .map((ex) => {
           switch (route.params.category) {
-            case 'Volume':
+            case 'Max Weight':
+              return {
+                value: Math.max(...ex.sets.map((set) => Number(set.weight))),
+                date: new Date(ex.sessionDate),
+                label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
+                  day: '2-digit',
+                  month: '2-digit'
+                })
+              };
+            case 'Average Weight':
+              const totalWeight = ex.sets.reduce(
+                (acc, set) => acc + Number(set.weight),
+                0
+              );
+              const averageWeight = totalWeight / ex.sets.length;
+              return {
+                value: averageWeight,
+                date: new Date(ex.sessionDate),
+                label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
+                  day: '2-digit',
+                  month: '2-digit'
+                })
+              };
+            default:
               return {
                 value: ex.sets.reduce(
                   (acc, set) => acc + Number(set.weight) * set.reps,
@@ -46,139 +67,27 @@ export const StatsScreen: React.FC<Props> = ({ route }) => {
                   month: '2-digit'
                 })
               };
-            case 'Max Weight':
-              return {
-                value: Math.max(...ex.sets.map((set) => Number(set.weight))),
-                date: new Date(ex.sessionDate),
-                label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
-                  day: '2-digit',
-                  month: '2-digit'
-                })
-              };
-            case 'Average Weight':
-              console.log('Sets:', ex.sets);
-              if (ex.sets && ex.sets.length > 0) {
-                const totalWeight = ex.sets.reduce((acc, set) => {
-                  console.log('Set:', set);
-                  return acc + (Number(set.weight) || 0);
-                }, 0);
-                const averageWeight = totalWeight / ex.sets.length;
-                return {
-                  value: averageWeight,
-                  date: new Date(ex.sessionDate),
-                  label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
-                    day: '2-digit',
-                    month: '2-digit'
-                  })
-                };
-              } else {
-                return {
-                  value: 0,
-                  date: new Date(),
-                  label: new Date().toLocaleDateString('en-GB', {
-                    day: '2-digit',
-                    month: '2-digit'
-                  })
-                };
-              }
-            case 'Number of Sets':
-              return {
-                value: ex.sets.length,
-                date: new Date(ex.sessionDate),
-                label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
-                  day: '2-digit',
-                  month: '2-digit'
-                })
-              };
-            case 'Number of Reps':
-              return {
-                value: ex.sets.reduce((acc, set) => acc + Number(set.reps), 0),
-                date: new Date(ex.sessionDate),
-                label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
-                  day: '2-digit',
-                  month: '2-digit'
-                })
-              };
-            case 'Reps per Set':
-              console.log('Processing Reps per Set for exercise:', ex.name);
-              if (ex.sets && ex.sets.length > 0) {
-                const setsWithReps = ex.sets.filter((set) => {
-                  const hasReps =
-                    set.reps !== undefined &&
-                    set.reps !== null &&
-                    !isNaN(Number(set.reps));
-                  console.log(
-                    `Checking set: ${JSON.stringify(set)}, hasReps: ${hasReps}`
-                  );
-                  return hasReps;
-                });
-                console.log('Filtered sets with reps:', setsWithReps);
-                if (setsWithReps.length > 0) {
-                  const totalReps = setsWithReps.reduce((acc, set) => {
-                    const reps = Number(set.reps); // Ensure conversion to Number
-                    console.log(
-                      `Accumulating reps, current set: ${JSON.stringify(
-                        set
-                      )}, converted reps: ${reps}`
-                    );
-                    return acc + reps;
-                  }, 0);
-                  const averageReps = totalReps / setsWithReps.length;
-                  console.log(
-                    `Total reps: ${totalReps}, Average: ${averageReps}`
-                  );
-                  return {
-                    value: averageReps,
-                    date: new Date(ex.sessionDate),
-                    label: new Date(ex.sessionDate).toLocaleDateString(
-                      'en-GB',
-                      {
-                        day: '2-digit',
-                        month: '2-digit'
-                      }
-                    )
-                  };
-                } else {
-                  console.log('No valid sets found for averaging reps.');
-                  return {
-                    value: 0,
-                    date: new Date(ex.sessionDate),
-                    label: new Date(ex.sessionDate).toLocaleDateString(
-                      'en-GB',
-                      {
-                        day: '2-digit',
-                        month: '2-digit'
-                      }
-                    )
-                  };
-                }
-              }
-              return null;
-
-            case 'Number of Sessions':
-              return {
-                value: 1, // Each exercise occurrence represents one session
-                date: new Date(ex.sessionDate),
-                label: new Date(ex.sessionDate).toLocaleDateString('en-GB', {
-                  day: '2-digit',
-                  month: '2-digit'
-                })
-              };
-
-            // Add more cases here for other categories
-
-            default:
-              return null;
           }
         })
-        .filter((item) => item !== null) // Remove null items
         .sort((a, b) => a.date.getTime() - b.date.getTime())
         .map((item) => ({ value: item.value, label: item.label }))
     : [];
+
+  // Determine the maximum number of labels to display
+  const maxLabels = 10;
+
+  // Calculate the interval between labels
+  const labelInterval = Math.ceil(chartData.length / maxLabels);
+
+  // Only include a label if its index is a multiple of the interval
+  const reducedLabels = chartData.filter(
+    (_, index) => index % labelInterval === 0
+  );
+
   console.log('Chart Data for Rendering:', chartData);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{route.params.category}</Text>
+      <Text style={styles.title}>Volume</Text>
 
       {isFetching ? (
         <View>
@@ -189,23 +98,40 @@ export const StatsScreen: React.FC<Props> = ({ route }) => {
         <Text>Error: {String(error)}</Text>
       ) : chartData && chartData.length > 0 ? (
         <LineChart
-          data={chartData}
-          isAnimated
-          color={'#B9EFF3'}
-          thickness={2}
-          hideDataPoints
-          dataPointsColor1="#3C748B"
-          focusedDataPointColor={'#3C748B'}
-          yAxisColor="#EBEFF1"
-          xAxisColor="#EBEFF1"
-          width={windowWidth * 0.71}
+          data={{
+            labels: reducedLabels.map((item) => item.label),
+            datasets: [
+              {
+                data: chartData.map((item) => item.value)
+              }
+            ]
+          }}
+          width={windowWidth * 0.95}
           height={windowHeight * 0.45}
-          secondaryYAxis={{}}
-          noOfSections={5}
-          animateOnDataChange
-          animationDuration={1000}
-          onDataChangeAnimationDuration={300}
-          yAxisTextStyle={{ color: 'lightgray' }}
+          chartConfig={{
+            backgroundColor: 'white',
+            backgroundGradientFrom: 'white',
+            backgroundGradientTo: 'white',
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            fillShadowGradient: 'rgba(185, 239, 243, 1)', // Change the color of the area below the line here
+            fillShadowGradientOpacity: 1,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: '3.5',
+              strokeWidth: '',
+              stroke: '',
+              fill: `rgba(0, 0, 0, 0.5)` // Change the color of the dots here
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
         />
       ) : (
         <Text>Please Enter and exercise name</Text>
